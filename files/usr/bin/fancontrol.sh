9fd210f4d7f658897826a43f91b2d831bbbeb0f3
lemi-aemi <muhamad.sulaemi@gmail.com>
2026-09-16 16:44:33 +0700

    Implement automatic fan speed control script
    Add a fan control script that adjusts fan speed based on SoC temperature.

--- files/usr/bin/fancontrol.sh
+++ files/usr/bin/fancontrol.sh
@@ -1,2 +1,15 @@
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
+#!/bin/sh
+# Skrip otomatis kontrol kecepatan kipas berdasarkan suhu SoC/System
+while true; do
+    TEMP=$(cat /sys/class/thermal/thermal_zone0/temp 2>/dev/null || echo 0)
+    TEMP_C=$((TEMP / 1000))
+
+    if [ $TEMP_C -gt 65 ]; then
+        echo 255 > /sys/class/hwmon/hwmon0/pwm1 2>/dev/null
+    elif [ $TEMP_C -gt 50 ]; then
+        echo 160 > /sys/class/hwmon/hwmon0/pwm1 2>/dev/null
+    else
+        echo 90 > /sys/class/hwmon/hwmon0/pwm1 2>/dev/null
+    fi
+    sleep 10
+done
